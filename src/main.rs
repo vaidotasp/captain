33e9fefcbd66a6a4103ee2e2d4b6b1c3976580df
Vaidotas Piekus <vaidotasp@gmail.com>
2023-08-09 13:21:55 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,51 @@
 use std::path::Path;
 
 use chrono::Datelike;
-use image::imageops;
+use image::{imageops, DynamicImage};
 
-// Captain image is 500x425
-
-// "static" means static lifetime which is to say that the value will live for the duration of the program. it is the longest possible lifetime.
-fn get_path() -> &'static str {
-    // weekly images
-    const MONDAY_PATH: &str = "src/monday.png";
-    const TUESDAY_PATH: &str = "src/tuesday.png";
-    const WEDNESDAY_PATH: &str = "src/wednesday.png";
-    const THURSDAY_PATH: &str = "src/thursday.png";
-    const FRIDAY_PATH: &str = "src/friday.png";
-    const SATURDAY_PATH: &str = "src/saturday.png";
-    const SUNDAY_PATH: &str = "src/sunday.png";
+fn get_file_image() -> DynamicImage {
+    // we basically jam all pngs into memory so that we can access at runtime.
+    let monday_bytes = include_bytes!("monday.png");
+    let tuesday_bytes = include_bytes!("tuesday.png");
+    let wednesday_bytes = include_bytes!("wednesday.png");
+    let thursday_bytes = include_bytes!("thursday.png");
+    let friday_bytes = include_bytes!("friday.png");
+    let saturday_bytes = include_bytes!("saturday.png");
+    let sunday_bytes = include_bytes!("sunday.png");
+    let monday = image::load_from_memory(monday_bytes).unwrap();
+    let tuesday = image::load_from_memory(tuesday_bytes).unwrap();
+    let wednesday = image::load_from_memory(wednesday_bytes).unwrap();
+    let thursday = image::load_from_memory(thursday_bytes).unwrap();
+    let friday = image::load_from_memory(friday_bytes).unwrap();
+    let saturday = image::load_from_memory(saturday_bytes).unwrap();
+    let sunday = image::load_from_memory(sunday_bytes).unwrap();
 
     let current_time = chrono::offset::Local::now();
     let weekday = current_time.date_naive().weekday();
+
     match weekday {
-        chrono::Weekday::Mon => return MONDAY_PATH,
-        chrono::Weekday::Tue => return TUESDAY_PATH,
-        chrono::Weekday::Wed => return WEDNESDAY_PATH,
-        chrono::Weekday::Thu => return THURSDAY_PATH,
-        chrono::Weekday::Fri => return FRIDAY_PATH,
-        chrono::Weekday::Sat => return SATURDAY_PATH,
-        chrono::Weekday::Sun => return SUNDAY_PATH,
+        chrono::Weekday::Mon => return monday,
+        chrono::Weekday::Tue => return tuesday,
+        chrono::Weekday::Wed => return wednesday,
+        chrono::Weekday::Thu => return thursday,
+        chrono::Weekday::Fri => return friday,
+        chrono::Weekday::Sat => return saturday,
+        chrono::Weekday::Sun => return sunday,
     }
 }
 
 fn main() {
-    // load png in memory and see how we can read it?
+    let cap_bytes = include_bytes!("captain.png");
+    let mut cap = image::load_from_memory(cap_bytes).unwrap();
 
-    // `open` returns a `DynamicImage` on success.
-    let mut cap = image::open("src/captain.png").unwrap();
-    let img = image::open(get_path()).unwrap();
-    let img2 = img.resize(120, 30, image::imageops::FilterType::Gaussian);
+    let img = get_file_image();
     // The dimensions method returns the images width and height.
+    let img2 = img.resize(120, 30, image::imageops::FilterType::Gaussian);
 
     //approximate location of speech bubble where we overlay the day of the week
     imageops::overlay(&mut cap, &img2, 195, 135);
-
-    let save_path = Path::new("src/new.png");
+    let save_path = Path::new("new.png");
     cap.save(save_path).unwrap();
+    println!("done");
     return;
 }
